fd036bbe6e015c5a87097b27006b19655fd216a5
n1kil <nikchernyavsky@bk.ru>
2025-05-18 20:31:35 +1000

    add def move

--- src/hero.py
+++ src/hero.py
@@ -1,9 +1,14 @@
-
+import pygame
 
 class Hero:
 
-    def __init__(self, health, damage, speed):
-
+    def __init__(self, health, damage, speed, x, y):
         self.health = health
         self.damage = damage
         self.speed = speed
+        self.x = x
+        self.y = y
+    
+    def move(self, x, y, speed):
+        x += speed
+        y += speed
